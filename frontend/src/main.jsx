2eefb19bdd6161215011fe4aff041f817ef22d47
Najamulhassan33383 <najmulhassan721@gmail.com>
2023-11-19 19:41:57 +0500

    completed the product screen page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,11 +13,13 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/" element={<HomeScreen />} />
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
