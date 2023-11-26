87241069e2e2a2d39e68db0adb547e0267810faa
Najamulhassan33383 <najmulhassan721@gmail.com>
2023-11-26 17:58:55 +0500

    made product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Products from "../models/productModel.js";
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Products.find({});
+  res.send(products);
+});
+
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Products.findById(req.params.id); // Add 'await' here
+
+  if (product) {
+    return res.send(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+export { getProducts, getProductById };
