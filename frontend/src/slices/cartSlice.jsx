cff049690b44cef33f0d12be4f0e24ea2e4f5f12
Najamulhassan33383 <najmulhassan721@gmail.com>
2023-11-29 22:03:10 +0500

    added the add to cart functionality for redux

--- frontend/src/slices/cartSlice.jsx
+++ frontend/src/slices/cartSlice.jsx
@@ -4,22 +4,53 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : {
       cartItems: [],
-      total: 0,
     };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
     addToCart: (state, action) => {
       const newItem = action.payload;
-      state.items.push(newItem);
-      state.total += newItem.price;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      if (existingItem) {
+        state.cartItems = state.items.map((item) =>
+          item.id === existingItem.id ? newItem : item
+        );
+      } else {
+        state.cartItems = [...state.items, newItem];
+      }
+
+      //calculate item price
+      state.itemsPrice = addDecimals(
+        state.items.reduce((total, item) => total + item.price * item.qty, 0)
+      );
+      //calculate shipping price, if order is over 100, shipping is free, else 10 dollars
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      //calculate tax price (15% tax)
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+
+      //calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      //store it in local storage
+      localStorage.setItem("cart", JSON.stringify(state));
     },
     removeFromCart: (state, action) => {
       const itemId = action.payload;
       state.items = state.items.filter((item) => item.id !== itemId);
-      state.total = state.items.reduce((total, item) => total + item.price, 0);
+      
     },
     clearCart: (state) => {
       state.items = [];
