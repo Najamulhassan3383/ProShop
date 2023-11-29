31ba7f3966c986eb6dbff0fe8928351e39a0aba5
Najamulhassan33383 <najmulhassan721@gmail.com>
2023-11-29 21:51:27 +0500

    added cart state in redux

---
+++ frontend/src/slices/cartSlice.jsx
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : {
+      cartItems: [],
+      total: 0,
+    };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const newItem = action.payload;
+      state.items.push(newItem);
+      state.total += newItem.price;
+    },
+    removeFromCart: (state, action) => {
+      const itemId = action.payload;
+      state.items = state.items.filter((item) => item.id !== itemId);
+      state.total = state.items.reduce((total, item) => total + item.price, 0);
+    },
+    clearCart: (state) => {
+      state.items = [];
+      state.total = 0;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice.js";
+import CartSliceReducer from "./slices/cartSlice.jsx";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: CartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
