2795d2eb28885afba5bdce063e1d5f0174aafe29
Najamulhassan33383 <najmulhassan721@gmail.com>
2023-12-14 22:16:42 +0500

    fixed some error order methods for fetcing orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,10 +9,10 @@ const addOrderItems = asyncHandler(async (req, res) => {
     orderItems,
     shippingAddress,
     paymentMethod,
-    // itemsPrice,
-    // taxPrice,
-    // shippingPrice,
-    // totalPrice,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
   } = req.body;
 
   if (orderItems && orderItems.length === 0) {
@@ -21,14 +21,20 @@ const addOrderItems = asyncHandler(async (req, res) => {
     return;
   } else {
     const order = new Order({
-      orderItems,
+      orderItems: orderItems.map((item) => {
+        return {
+          ...item,
+          product: req._id,
+          _id: undefined,
+        };
+      }),
       user: req.user._id,
       shippingAddress,
       paymentMethod,
-      // itemsPrice,
-      // taxPrice,
-      // shippingPrice,
-      // totalPrice,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
     });
 
     const createdOrder = await order.save();
@@ -42,7 +48,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access  Private
 const getUserOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
-  res.json(orders);
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by id
@@ -54,7 +60,7 @@ const getOrderById = asyncHandler(async (req, res) => {
     "name email"
   );
   if (order) {
-    res.json(order);
+    res.status(200).json(order);
   } else {
     res.status(404);
     throw new Error("Order not found");
